15144e8307c73424a82935993a8df595ab87383e
byana01 <118729264+byana01@users.noreply.github.com>
2022-11-24 13:18:09 +0100

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,3 +1,10 @@
 class profile::web {
   include nginx
+  
+  tomcat::install { '/opt/tomcat':
+  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.x/bin/apache-tomcat-8.0.x.tar.gz',
+}
+tomcat::instance { 'default':
+  catalina_home => '/opt/tomcat',
+}
 }
