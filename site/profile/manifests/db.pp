06ca6458a5b28fb21017581a46774a7bf4ecd324
byana01 <118729264+byana01@users.noreply.github.com>
2022-11-24 14:06:43 +0100

    Update db.pp

--- site/profile/manifests/db.pp
+++ site/profile/manifests/db.pp
@@ -1,10 +1,10 @@
 class profile::db{
 include java
 
-tomcat::install { '/root/tomcat':
+tomcat::install { '/apps/tomcat':
   source_url => 'https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.84/bin/apache-tomcat-8.5.84.tar.gz',
 }
 tomcat::instance { 'default':
-  catalina_home => '/root/tomcat',
+  catalina_home => '/apps/tomcat',
 }
 }
